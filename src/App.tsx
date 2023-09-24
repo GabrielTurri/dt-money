98875dacc3ad376051edc8b475777cdfd2361e88
Gabriel Turri <ga-briel.t.a@hotmail.com>
2023-09-24 11:53:16 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle>
+        
+      </GlobalStyle>
+      
+      <h1>Hello World</h1>
+    </ThemeProvider>
+    )
+}
