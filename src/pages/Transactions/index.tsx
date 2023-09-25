9e65f0afecbfbb637301b0bef167afd176e72a6a
Gabriel Turri <ga-briel.t.a@hotmail.com>
2023-09-25 17:44:53 -0300

    creating accessible modal

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from '../../components/Summary'
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -7,8 +8,9 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
-
+      
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
