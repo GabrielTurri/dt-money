3d64522a30f9237062a5f4f51017cba195438427
Gabriel Turri <ga-briel.t.a@hotmail.com>
2023-09-27 00:53:46 -0300

    creating Transactions Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Transactions } from './pages/Transactions'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 
 export function App() {
   return (
@@ -9,8 +10,9 @@ export function App() {
       <GlobalStyle>
         
       </GlobalStyle>
-      
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
     )
 }
---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,42 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType)
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] =  useState<Transaction[]>([])
+ 
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json();
+    
+    setTransactions(data)
+  }
+  
+  useEffect(() => {
+    
+    loadTransactions()
+  }, [])
+  
+  return (
+    <TransactionsContext.Provider value={{transactions}}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
