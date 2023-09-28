6819054be5c485e14e36e2df71e1cbfaa3de9516
Gabriel Turri <ga-briel.t.a@hotmail.com>
2023-09-28 15:32:33 -0300

    creating new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,8 @@ import * as z from 'zod';
 
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
 import { Controller, useForm } from "react-hook-form";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -16,11 +18,14 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal(){
+  const { createTransaction } = useContext(TransactionsContext)
+
   const {
     control,
     register,
     handleSubmit,
-    formState: {isSubmitting}
+    formState: {isSubmitting},
+    reset
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
     defaultValues: { 
@@ -29,9 +34,16 @@ export function NewTransactionModal(){
   })
   
   async function handleCreateNewTransaction(data: NewTransactionFormInputs){
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    const { description, price, category, type } = data;
+    
+    await createTransaction({
+      description, 
+      price, 
+      category, 
+      type
+    })
 
-    console.log(data)
+    reset()
   }
   
   return (
